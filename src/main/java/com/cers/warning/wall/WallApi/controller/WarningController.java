56942ce8100588ab4c1f1add442d0c529fe5e7a3
Freitas <myller.c.freitas@accenture.com>
2020-01-01 23:39:49 -0300

    permitindo acesso diferentes fontes

--- src/main/java/com/cers/warning/wall/WallApi/controller/WarningController.java
+++ src/main/java/com/cers/warning/wall/WallApi/controller/WarningController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.cers.warning.wall.WallApi.dto.WarningPageableDTO;
 import com.cers.warning.wall.WallApi.persistence.WarningEntity;
 import com.cers.warning.wall.WallApi.service.IWarningService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/aviso")
 public class WarningController {
